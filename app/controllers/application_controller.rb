567ac4d1c93c49c82023c4ec017279e2b0d20bf1
Alina Moskieva <alina.moskieva@flatstack.com>
2018-02-13 12:30:31 +0300

    Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < Telegram::Bot::UpdatesController
     include Telegram::Bot::UpdatesController::MessageContext
     include Authentication
+
+    def respond_with_message(text)
+      respond_with :message, text: text, parse_mode: "markdown"
+    end
+
+    def respond_with_message_and_buttons(text, buttons)
+      respond_with :message, text: text, parse_mode: "markdown", reply_markup: { inline_keyboard: buttons }
+    end
 end
--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -6,29 +6,28 @@ class TelegramWebhooksController < ApplicationController
 
   def add_serial(cmd = nil, *)
     result = Serials::Add.call(data: payload, user: current_user).result
-    respond_with :message, text: result, parse_mode: "markdown"
+    respond_with_message(result)
   end
 
   def my_serials
     result = Serials::MyList.call(data: payload, user: current_user).result
-    respond_with :message, text: result, parse_mode: "markdown"
+    respond_with_message(result)
   end
 
   def remove_serial(cmd = nil, *)
     result = Serials::Remove.call(data: payload, user: current_user).result
-    respond_with :message, text: result, parse_mode: "markdown"
+    respond_with_message(result)
   end
 
   def movies(cmd = nil, *)
     result = Movies::New.call.list
-    respond_with :message, text: result, parse_mode: "markdown"
+    respond_with_message(result)
   end
 
   def most_popular(cmd = nil, *)
     result = Movies::Popular.call
-    respond_with :message, text: result.list, parse_mode: "markdown", reply_markup: { inline_keyboard: result.buttons }
+    respond_with_message_and_buttons(result.list, result.buttons)
   end
-
 
   def callback_query(data)
     respond_with :message, text: data.to_s, parse_mode: "markdown"
